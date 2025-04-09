83bfcbd2eacebf6bf41e5d0e261eec968a378983
ChazzTheCrab <133388927+ChazzTheCrab@users.noreply.github.com>
2025-04-09 15:50:09 +0300

    adgasgh
    afsafsf

--- TROLLĪTIS.py
+++ TROLLĪTIS.py
@@ -4,7 +4,7 @@ tagad = datetime.datetime.now()
 datums = tagad.strftime("%Y-%m-%d")
 
 def galva():
-    with sqlite3.connect("tkrekli.db") as conn:
+    with sqlite3.connect("satiksmes_laiki.db") as conn:
         c = conn.cursor()
 
         TROLLIS(c)
@@ -16,41 +16,42 @@ def TROLLIS(c):
     atbilde = ["do", "re", "mi", "fa", "sol"]
     while True:
         jaut1 = input("Par kuras pieturas laikiem gribat uzzināt? Ja nezinat par kuru rakstat N: ")
-        for pieturas_nosaukums in atbilde:
-            pietura = pieturas_nosaukums
-            if jaut1 == "N":
-                jaut2 = input("TRAMVAJS\nAUTOBUSS\nTROLEJBUSS\nPar kuru transportu gribat uzzināt: ")
-                if jaut2 == "TRAMVAJS":
-                    #ŠEIT SARAKSTU AR TRAMVAJA NUMMURIEM
-                    jaut_tran = input("Par kuru tramvaju gribat uzzināt: ")
-                    datu_bazite_trans(c)
+        while True:
+            for pieturas_nosaukums in atbilde:
+                pietura = pieturas_nosaukums
+                if jaut1 == "N":
+                    jaut2 = input("TRAMVAJS\nAUTOBUSS\nTROLEJBUSS\nPar kuru transportu gribat uzzināt: ")
+                    if jaut2 == "TRAMVAJS":
+                        #ŠEIT SARAKSTU AR TRAMVAJA NUMMURIEM
+                        jaut_tran = input("Par kuru tramvaju gribat uzzināt: ")
+                        datu_bazite_trans(c)
+                        break
+                    elif jaut2 == "AUTOBUSS":
+                        #ŠEIT SARAKSTU AR AUTOBUSA NUMMURIEM
+                        jaut_tran = input("Par kuru autobusu gribat uzzināt: ")
+                        datu_bazite_trans(c)
+                        break
+                    elif jaut2 == "TROLEJBUSS":
+                        #ŠEIT SARAKSTU AR TROLEJBUSA NUMMURIEM
+                        jaut_tran = input("Par kuru trolejbusu gribat uzzināt: ")
+                        datu_bazite_trans(c)
+                        break
+                elif jaut1 == pietura:
+                    print("čiki puki")
+                    datu_bazite_pietura(c)
                     break
-                elif jaut2 == "AUTOBUSS":
-                    #ŠEIT SARAKSTU AR AUTOBUSA NUMMURIEM
-                    jaut_tran = input("Par kuru autobusu gribat uzzināt: ")
-                    datu_bazite_trans(c)
-                    break
-                elif jaut2 == "TROLEJBUSS":
-                    #ŠEIT SARAKSTU AR TROLEJBUSA NUMMURIEM
-                    jaut_tran = input("Par kuru trolejbusu gribat uzzināt: ")
-                    datu_bazite_trans(c)
-                    break
-            elif jaut1 == pietura:
-                print("čiki puki")
-                datu_bazite_pietura(c)
-                break
             else:
                 print("Nepareizi ievadīts vai nav tādas pieturas")
+                #vaig lai atkārto jautajumu
+            break
         break
                 
 
 
 
-
-
-
 def datu_bazite_pietura(c):
-    aaaa = 0
+    #ŠEIT LAI PARADA SARAKSTU AR TAJA PIETURĀ PIENĀKOŠIEM TRANSPORTIEM ĶIP 
+    jaut_tran = input("")
 
 
 
